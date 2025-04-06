5dfe315b088f1b9d60d3200951a32fd9b9c749d6
danieljaramillo24 <daniel.jaramillo24@udea.edu.co>
2025-04-06 16:10:10 -0500

    Creados los métodos suma promedio, máximo y mínimio

--- src/FrmEstadistica.java
+++ src/FrmEstadistica.java
@@ -18,6 +18,8 @@ public class FrmEstadistica extends JFrame {
     // Declaración de la variables de forma global fuera del constructor.
     private JTextField txtDato;
     private JList lstMuestra;
+    private JComboBox cmbEstadistica;
+    private JTextField txtEstadistica;
 
     // Método constructor
     public FrmEstadistica() {
@@ -72,7 +74,7 @@ public class FrmEstadistica extends JFrame {
         getContentPane().add(btnEstadistica);
 
         // JComboBox permite seleccionar un elemento de una lista de opciones.
-        JComboBox cbmEstadistica = new JComboBox();
+        cmbEstadistica = new JComboBox();
 
         // Definimos las opciones disponibles en el JComboBox.
         // Se declara un array de Strings con las opciones.
@@ -82,21 +84,23 @@ public class FrmEstadistica extends JFrame {
         // opciones.
         // Esto permite asignar el conjunto de datos al JComboBox.
         DefaultComboBoxModel dcm = new DefaultComboBoxModel(opciones);
-        cbmEstadistica.setModel(dcm); // Asociamos el modelo al JComboBox.
+        cmbEstadistica.setModel(dcm); // Asociamos el modelo al JComboBox.
 
         // Configuramos la posición y el tamaño del JComboBox dentro del contenedor.
-        cbmEstadistica.setBounds(110, 200, 100, 25);
+        cmbEstadistica.setBounds(110, 200, 100, 25);
 
         // Agregamos el JComboBox al contenido del contenedor principal.
-        getContentPane().add(cbmEstadistica);
+        getContentPane().add(cmbEstadistica);
 
         // Esta es una caja de texto de salida.
-        JTextField txtEstadistica = new JTextField();
+        txtEstadistica = new JTextField();
         txtEstadistica.setBounds(220, 200, 100, 25);
 
         // Deshabilita la habilidad de digitar en una caja de texto.
-        txtEstadistica.setEnabled(false);
-        getContentPane().add(txtDato);
+        // set editable es otra forma diferente de, se puede editar la caja y mejora al
+        // go el aspecto
+        txtEstadistica.setEditable(false);
+        getContentPane().add(txtEstadistica);
 
         // Crear eventos de los botones.
         // addActionListener => Se crea a la espera de que suceda un evento.
@@ -128,7 +132,7 @@ public class FrmEstadistica extends JFrame {
 
             @Override
             public void actionPerformed(ActionEvent e) {
-                Estadistica();
+                calcularEstadistica();
             }
 
         });
@@ -147,23 +151,27 @@ public class FrmEstadistica extends JFrame {
     private int totalDatos = 0;
 
     private void mostrarDatos() {
-        // Declaramos otro vector de string, al que queremos ir agregando lo que ya tenemos en muestra.
+        // Declaramos otro vector de string, al que queremos ir agregando lo que ya
+        // tenemos en muestra.
         String[] strMuestra = new String[totalDatos];
-        for(int i=0;i<totalDatos;i++){
+        for (int i = 0; i < totalDatos; i++) {
             strMuestra[i] = String.valueOf(muestra[i]);
         }
         // Los JList tiene el método setListData para que le permitan agregar datos.
-        // Se actualiza el contenido de la lista "lstMuestra" con los datos almacenados en el arreglo "strMuestra".
+        // Se actualiza el contenido de la lista "lstMuestra" con los datos almacenados
+        // en el arreglo "strMuestra".
         lstMuestra.setListData(strMuestra);
 
-
     }
+
     private void agregarDato() {
         if (totalDatos < MAXIMO_DATOS) {
-            // Agregamos el dato que digitamos en la cada de texto para esto, al vector de reales muestra y aumentamos el contador de los datos para actualizar la posición.
+            // Agregamos el dato que digitamos en la cada de texto para esto, al vector de
+            // reales muestra y aumentamos el contador de los datos para actualizar la
+            // posición.
             // Utilizamos para obtener ese texto de la caja el método getText
             muestra[totalDatos] = Double.parseDouble(txtDato.getText());
-            totalDatos ++;
+            totalDatos++;
             // Instanciamos el método para mostar los datos
             mostrarDatos();
         } else {
@@ -174,10 +182,113 @@ public class FrmEstadistica extends JFrame {
     }
 
     private void quitarDato() {
-        JOptionPane.showMessageDialog(null, "Hizo click en Quitar.");
+        // getSelectedIndex sirve para leer listas debe tener elementos.
+        // Al tocar en elemento de lstMuestra en pantalla nos retorna el Index de ese
+        // elemento en la lista si no se selecciono nada dispara el else.
+
+        // Nota. Eliminar consiste en recorrer el elemento con base a su indice,
+        // eliminarlo y eliminar una posición de la lista. Las posiciones se desplazan a
+        // la izquierda.
+        if (lstMuestra.getSelectedIndex() >= 0) {
+            // Recorre los elementos trayendo el indice hasta la íltima posición
+            // Con paso de uno.
+            // Vamos hasta la penultima para poder que no salgamos del vector.
+            for (int i = lstMuestra.getSelectedIndex(); i < totalDatos - 1; i++) {
+                // Muevo el elemento siguiente a la posición anterior en el array
+                muestra[i] = muestra[i + 1];
+            }
+            totalDatos--; // Como vamos a eliminar una posición debemos reducir el tamaño del vector.
+            // Volvemos a mostrar el array actualizado.
+            mostrarDatos();
+        } else {
+            JOptionPane.showMessageDialog(null, "Debe seleccionar el objeto a retirar.");
+        }
     }
 
-    private void Estadistica() {
-        JOptionPane.showMessageDialog(null, "Hizo click en Calcular Estadistica.");
+    private double sumatoria() {
+        // Definimos una variable acumuladora
+        double suma = 0;
+        for (int i = 0; i < totalDatos; i++) {
+            suma += muestra[i];
+        }
+        return suma;
+    }
+
+    private double desviacionEstandar() {
+        // método para la desviación estandar
+        // Guardamos el promedio que es constante
+        if (totalDatos > 1) {
+            double promedioActual = promedio();
+            double suma = 0;
+            for (int i = 0; i < totalDatos; i++) {
+                suma += Math.abs(promedioActual - muestra[i]);
+            }
+            return suma / (totalDatos - 1);
+        } else {
+            return 0;
+        }
+    }
+
+    // Como hallar el maximo de un conjunto de datos.
+    // Tomamos un dato y decimos que es el mayor y lo comparamos con el siguiente si
+    // resulta ser mayor, volvemos a este el mayor, luego pasamos al siguiente hasta
+    // todos los datos.
+    private double maximo() {
+        if (totalDatos > 0) {
+            double actualMaximo = muestra[0];
+            for (int i = 1; i < totalDatos; i++) {
+                if (muestra[i] > actualMaximo) {
+                    actualMaximo = muestra[i];
+                }
+            }
+            return actualMaximo;
+        } else {
+            return 0;
+        }
+    }
+
+    private double minimo() {
+        {
+            if (totalDatos > 0) {
+                double actualMinimo = muestra[0];
+                for (int i = 1; i < totalDatos; i++) {
+                    if (muestra[i] < actualMinimo) {
+                        actualMinimo = muestra[i];
+                    }
+                }
+                return actualMinimo;
+            } else {
+                return 0;
+            }
+        }
+    }
+
+    private double promedio() {
+        return totalDatos > 0 ? sumatoria() / totalDatos : 0;
+        // Recordemos se lee, si total datos es mayor que 0 (? == entonces) sumatoria()
+        // / totalDatos esto es el promedio, :0 == Sino retorne 0
+    }
+
+    private void calcularEstadistica() {
+        switch (cmbEstadistica.getSelectedIndex()) {
+            case 0:
+                // A la caja de texto le vamos a setear asinar, el valor de la sumatoria.
+                // Convertimos el tipo de dato la caja de texto solo recibe string.
+                txtEstadistica.setText(String.valueOf(sumatoria()));
+                break;
+            case 1:
+                txtEstadistica.setText(String.valueOf(promedio()));
+                break;
+            case 2:
+                txtEstadistica.setText(String.valueOf(desviacionEstandar()));
+                break;
+            case 3:
+                txtEstadistica.setText(String.valueOf(maximo()));
+                break;
+            case 4:
+                txtEstadistica.setText(String.valueOf(minimo()));
+                break;
+
+        }
     }
 }
